3c1f1a204e89ded1902b6a910b76eed42bcf40e2
Kyle Isom <kyle@imap.cc>
2020-07-31 09:19:10 -0700

    fix linux bullshit

--- srm/config.sh
+++ srm/config.sh
@@ -12,7 +12,7 @@ OPSYS=$(uname -s)
 
 echo "Configuring for ${OPSYS}..."
 if [ "x${OPSYS}" = "xLinux" ]; then
-    OS_CFLAGS="-D_BSD_SOURCE -D_POSIX_SOURCE -D_XOPEN_SOURCE"
+    OS_CFLAGS="-D_DEFAULT_SOURCE -D_POSIX_SOURCE -D_XOPEN_SOURCE"
 elif [ "x${OPSYS}" = "xDarwin" ]; then
     OS_CFLAGS="-D_DARWIN_C_SOURCE"
 else
